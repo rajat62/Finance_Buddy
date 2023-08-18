cabb971326342afd4f595564307f1839b7868957
rajat62 <parmitachoudharyrck@gmail.com>
2023-08-18 22:25:45 +0530

    added year graph

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import moment from "moment";
+
+import Chart from 'chart.js/auto';
+import { Bar, Line } from "react-chartjs-2";
+
 const History = () => {
   const { allTransactions } = useSelector((state) => state.expense);
 
@@ -30,6 +34,73 @@ const History = () => {
 
   }
 
+  const yearData= [];
+  allTransactions.map((item) => {
+    const year = moment(item.date).format("YY");
+    const existingYear = yearData.find(
+      (entry) => moment(entry.year).format("YY") === year
+    );
+  
+    if (existingYear) {
+      existingYear.data.push(item); 
+    } else {
+      yearData.push({ year: item.date, data: [item] });
+    }
+  });
+
+  yearData.forEach((item) => {
+    let income = 0; 
+    let expense = 0; 
+    item.data.forEach((ele) => {
+      if (ele.paymentType === "income") {
+        income += ele.amount;
+      } else {
+        expense += ele.amount;
+      }
+    });
+  
+    item.totalIncome = income;
+    item.totalExpense = expense;
+  });
+
+
+  const ChartData = {
+    labels: yearData.map((item) => moment(item.year).format("YY")), // Labels for X-axis
+    datasets: [
+      {
+        data: yearData.map((item) => item.totalIncome), // Income data
+        label: "Income",
+        borderColor: "#007bff",
+        fill: true,
+        lineTension: 0.5,
+      },
+      {
+        data: yearData.map((item) => item.totalExpense), // Expense data
+        label: "Expense",
+        borderColor: "#ff3333",
+        backgroundColor: "rgba(255, 0, 0, 0.5)",
+        fill: true,
+        lineTension: 0.5,
+      },
+    ],
+  };
+
+  const options = {
+  
+    plugins: {
+      title: {
+        display: true,
+        text: "Expense Tracker",
+        fontSize: 20,
+        padding: 20,
+        maintainAspectRatio: true,
+      },
+      legend: {
+        display: true, // Is the legend shown?
+        position: "top", // Position of the legend.
+      },
+    },
+  };
   return (
     <div
       style={{ height: "80%%", borderRadius: ".8rem" , background: "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)", fontFamily:" 'Inconsolata', monospace "}}
@@ -72,6 +143,8 @@ const History = () => {
 
       </div>
         <button onClick={handleheaders}>Download CSV</button>
+
+        <Bar options={options} data={ChartData} width={"100%"} height={"50%"} />
     </div>
   );
 };
